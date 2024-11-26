722061fd1a9e218814d8f8c3be81ec37d1eefe95
Ahmed Abdelaziz <ahmeddev101@gmail.com>
2024-11-26 14:48:45 +0200

    footer links

--- app/views/layout/footer-2.php
+++ app/views/layout/footer-2.php
@@ -8,12 +8,12 @@
                 class="icon icon-carce-heart"></i></a>
             </div>
             <div class="user-event user-event--center">
-                <a area-label="event link icon" href="<?= BASE_URL . '/'; ?>" class="event-btn-link"><i
+                <a area-label="event link icon" href="<?= BASE_URL . '/offer'; ?>" class="event-btn-link"><i
                 class="icon icon-carce-plus"></i></a>
             </div>
             <div class="user-event user-event--right">
-                <a area-label="order icon" href="<?= BASE_URL . '/offer'; ?>" class="event-btn-link"><i
-                class="icon icon-carce-compare"></i></a>
+                <a area-label="order icon" href="<?= BASE_URL . '/notification'; ?>" class="event-btn-link"><i
+                class="icon icon-carce-bell"></i></a>
                 <a area-label="chat icon" href="<?= BASE_URL . '/chat/index/'; ?>" class="event-btn-link"><i
                 class="icon icon-carce-bubbles2"></i></a>
             </div>
